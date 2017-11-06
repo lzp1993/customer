86bcfd81283e40c446a7bdde7fa00fe374381f03
lizhipeng <lizhipeng@lianjia.com>
2017-11-06 18:42:11 +0800

    Initial commit

---
+++ src/main/java/org/lzp/model/Customer.java
@@ -0,0 +1,58 @@
+package org.lzp.model;
+
+public class Customer {
+    private String name;
+    private long id;
+    private String contact;
+    private String telephone;
+    private String email;
+    private String remark;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
